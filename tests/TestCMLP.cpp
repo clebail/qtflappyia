75445399ba32ca023f7230a04383c544a162dda0
Corentin Le Bail <corentin@kyxar.fr>
2026-06-09 13:54:54 +0200

    Fin 1er TU

--- tests/TestCMLP.cpp
+++ tests/TestCMLP.cpp
@@ -1,3 +1,4 @@
+#include <QVector>
 #include <math.h>
 #include "TestCMLP.h"
 
@@ -10,40 +11,82 @@ void TestCMLP::cleanupTestCase(void) {
 }
 
 void TestCMLP::testGradient(void) {
+    const double eps = 1e-4, eta = 1.0, cible = 1.0, tol = 1e-6;
+    const int action = 0;
     double inputs[FLAPPY_NB_INPUTS];
-    double wOrig;
-    double epsilon = 0.0001;
-    double Q, QOrig;
-    double lPlus, lMinus;
-    double gradNum, gradAnalytique;
-    double cible = 1;
+    QVector<CNeurone *>neurones;
+    int nbGenes = 0;
+    int idxGene = 0;
+    double *genes, *gradAna;
 
     for(int i=0;i<FLAPPY_NB_INPUTS;i++) {
         inputs[i] = 0.5;
     }
-    cmlp->forward(inputs);
 
-    wOrig = cmlp->getNeuroneSortie(0)->getGene(0);
-    cmlp->getNeuroneSortie(0)->setGene(0, wOrig + epsilon);
-    cmlp->forward(inputs);
-    Q = cmlp->getQ(0);
-    lPlus = 0.5 * (Q - cible) * (Q - cible);
-    cmlp->getNeuroneSortie(0)->setGene(0, wOrig);
-
-    wOrig = cmlp->getNeuroneSortie(0)->getGene(0);
-    cmlp->getNeuroneSortie(0)->setGene(0, wOrig - epsilon);
-    cmlp->forward(inputs);
-    Q = cmlp->getQ(0);
-    lMinus = 0.5 * (Q - cible) * (Q - cible);
-    cmlp->getNeuroneSortie(0)->setGene(0, wOrig);
-
-    gradNum = (lPlus - lMinus) / (2 * epsilon);
+    nbGenes += cmlp->getNeuroneSortie(action)->getNbGene();
+    neurones << cmlp->getNeuroneSortie(action);
+    for(int i=0;i<FLAPPY_NB_HIDDEN;i++) {
+        nbGenes += cmlp->getNeuroneCache(i)->getNbGene();
+        neurones << cmlp->getNeuroneCache(i);
+    }
 
     cmlp->forward(inputs);
-    QOrig = cmlp->getQ(0);
-    gradAnalytique = QOrig - cible;
 
-    QVERIFY(fabs(gradNum - gradAnalytique) < 0.000001);
+    // Snapshot des poids
+    idxGene = 0;
+    genes = new double[nbGenes];
+    gradAna = new double[nbGenes];
+    for(int i=0;i<neurones.count();i++) {
+        for(int j=0;j<neurones.at(i)->getNbGene();j++, idxGene++) {
+            genes[idxGene] = neurones.at(i)->getGene(j);
+        }
+    }
+
+    cmlp->backward(action, cible, eta);
+
+    // Calcul du grad analytic
+    idxGene = 0;
+    for(int i=0;i<neurones.count();i++) {
+        for(int j=0;j<neurones.at(i)->getNbGene();j++, idxGene++) {
+            gradAna[idxGene] = (genes[idxGene] - neurones.at(i)->getGene(j)) / eta;
+        }
+    }
+
+    // Restauration des poids
+    idxGene = 0;
+    for(int i=0;i<neurones.count();i++) {
+        for(int j=0;j<neurones.at(i)->getNbGene();j++, idxGene++) {
+            neurones.at(i)->setGene(j, genes[idxGene]);
+        }
+    }
+
+    idxGene = 0;
+    for(int i=0;i<neurones.count();i++) {
+        for(int j=0;j<neurones.at(i)->getNbGene();j++, idxGene++) {
+            double gene0 = neurones.at(i)->getGene(j);
+            double lPlus, lMinus;
+            double gradNum;
+            double Q;
+
+            neurones.at(i)->setGene(j, gene0 + eps);
+            cmlp->forward(inputs);
+            Q = cmlp->getQ(action);
+            lPlus = 0.5 * (Q - cible) * (Q - cible);
+
+            neurones.at(i)->setGene(j, gene0 - eps);
+            cmlp->forward(inputs);
+            Q = cmlp->getQ(action);
+            lMinus = 0.5 * (Q - cible) * (Q - cible);
+
+            neurones.at(i)->setGene(j, gene0);
+
+            gradNum = (lPlus - lMinus) / (2 * eps);
+            QVERIFY2(fabs(gradNum - gradAna[idxGene]) < tol, qPrintable(QString("idxGent=%1 num=%2 ana=%3 diff=%4").arg(idxGene).arg(gradNum).arg(gradAna[idxGene]).arg(fabs(gradNum - gradAna[idxGene]))));
+        }
+    }
+
+    delete[] genes;
+    delete[] gradAna;
 }
 
 QTEST_APPLESS_MAIN(TestCMLP)
